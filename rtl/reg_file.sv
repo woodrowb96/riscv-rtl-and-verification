612580ee19cedf1c4f175af279577ee69a03bb03
woodrowb <brenwoodr96@gmail.com>
2026-02-11 19:00:35 -0800

    REG_FILE: Modified the rtl so that it uses the defs package

--- rtl/reg_file.sv
+++ rtl/reg_file.sv
@@ -5,19 +5,21 @@ Control:
   wr_en:    active high write enable signal
 
 Input:
-  rd_reg_1: 5 bit read reg number
-  rd_reg_2: 5 bit read reg number
+  rd_reg_1: address for read data 1
+  rd_reg_2: address for read data 2
 
-  wr_reg:   5 bit write reg number
-  wr_data:  32 bit write reg data, writen into wr_reg on rising clk edge
+  wr_reg:   adress we are writting to
+  wr_data:  write data, writen into wr_reg on rising clk edg
 
 Output:
-  rd_data_1:  32 bits of read data read out from rd_reg_1
-  rd_data_2:  32 bits of read data read out from rd_reg_2
+  rd_data_1:  read data read out from rd_reg_1
+  rd_data_2:  read data read out from rd_reg_2
 
 Note:
   Register x0 (reg_file[0]) always returns '0
 */
+import riscv_32i_defs_pkg::*;
+
 module reg_file (
   input logic clk,
 
@@ -25,23 +27,23 @@ module reg_file (
   input logic wr_en,
 
   //input
-  input logic [4:0] rd_reg_1,
-  input logic [4:0] rd_reg_2,
+  input rf_addr_t rd_reg_1,
+  input rf_addr_t rd_reg_2,
 
-  input logic [4:0] wr_reg,
-  input logic [31:0] wr_data,
+  input rf_addr_t wr_reg,
+  input word_t wr_data,
 
   //output
-  output logic [31:0] rd_data_1,
-  output logic [31:0] rd_data_2
+  output word_t rd_data_1,
+  output word_t rd_data_2
 );
   //32 x 32'b registers, x0 initialized to 0
-  logic [31:0] reg_file [0:31];
-  initial reg_file[0] = '0;
+  word_t reg_file [0:RF_DEPTH-1];
+  initial reg_file[X0] = '0;
 
   //writting into reg file
   always_ff @(posedge clk) begin
-    if(wr_en && (wr_reg != '0)) begin   //if wr_en and not writting to x0, then write
+    if(wr_en && (wr_reg != X0)) begin   //if wr_en and not writting to x0, then write
       reg_file[wr_reg] <= wr_data;
     end
   end
@@ -49,7 +51,7 @@ module reg_file (
   //reading from reg file
   always_comb begin
     //output 0 out from x0, else output data stored in register
-    rd_data_1 = (rd_reg_1 == '0) ? '0 : reg_file[rd_reg_1];
-    rd_data_2 = (rd_reg_2 == '0) ? '0 : reg_file[rd_reg_2];
+    rd_data_1 = (rd_reg_1 == X0) ? '0 : reg_file[rd_reg_1];
+    rd_data_2 = (rd_reg_2 == X0) ? '0 : reg_file[rd_reg_2];
   end
 endmodule
