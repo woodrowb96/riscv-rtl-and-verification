d245a2fb04b76c39b721e561166aef2da24dd6b9
woodrowb <brenwoodr96@gmail.com>
2026-03-09 09:58:19 -0700

    COMMON: added a base transaction class

---
+++ verify/common/base_transaction_pkg.sv
@@ -0,0 +1,9 @@
+/*
+    Virtual base transaction class providing users can derive there own transaction from.
+*/
+package base_transaction_pkg;
+  virtual class base_transaction;
+    pure virtual function bit compare(base_transaction other);
+    pure virtual function void print(string msg = "");
+  endclass
+endpackage
