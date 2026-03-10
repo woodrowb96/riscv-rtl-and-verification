c368bdeeba413a0db7cd55a1d18e4d539fc99a16
woodrowb <brenwoodr96@gmail.com>
2026-03-09 17:05:45 -0700

    added id support to the base_transaction class

--- verify/common/base_transaction_pkg.sv
+++ verify/common/base_transaction_pkg.sv
@@ -2,8 +2,20 @@
     Virtual base transaction class providing users can derive there own transaction from.
 */
 package base_transaction_pkg;
+
   virtual class base_transaction;
+    static local int next_id = 0;
+    int id;
+
     pure virtual function bit compare(base_transaction other);
     pure virtual function void print(string msg = "");
+
+    protected function new();
+      this.id = next_id;
+      next_id++;
+    endfunction
   endclass
+
+
+
 endpackage
