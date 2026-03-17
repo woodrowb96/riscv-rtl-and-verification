2ab69a823c204b6f5e4e004c0138b520d64e7bca
woodrowb <brenwoodr96@gmail.com>
2026-03-16 21:02:19 -0700

    IMM_GEN: added a default case to the rtl
    this is to make sure we dont get latches during synthesis

--- rtl/imm_gen.sv
+++ rtl/imm_gen.sv
@@ -24,6 +24,9 @@ Output:
       - J-type:
                 - encoded immediate split across instruction
                 - imm = {{11{inst[31]}}, inst[31], inst[19:12], inst[20], inst[30:21], 1'b0}
+
+  Note: Invalid opcodes/instructions
+    - Output when this module gets passed an invalid opcode/instruction is undefined.
 */
 import rv32i_defs_pkg::*;
 
@@ -63,6 +66,11 @@ module imm_gen(
       OP_JAL: begin
         imm = {{11{inst[31]}}, inst[31], inst[19:12], inst[20], inst[30:21], 1'b0};
       end
+      //invalid opcodes
+      default: begin
+        imm = 'x; //output x's, behavior is undefined
+      end
     endcase
   end
+
 endmodule
